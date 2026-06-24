74d102d7fbbd78b6cf94a1b0ea46a5ab1ed98bd9
Madhav Ganesh <madhavganesh48@gmail.com>
2026-06-24 15:36:46 +0300

    Add files via upload

---
+++ WeatherEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using Platform.Services;
+
+namespace Platform
+{
+    public class WeatherEndpoint
+    {
+        public static async Task Endpoint(HttpContext context)
+        {
+            await TextResponseFormatter.Singleton.Format(context, "Endpoint Class: It is cloudy in Milan");
+
+        }
+    }
+}
